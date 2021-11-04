deb3c4f743d070b0c0c89037148ec30e09e52a3d
tscuite <tscuite@qq.com>
2021-11-04 17:09:56 +0800

    1

--- controllers/boy_controller.go
+++ controllers/boy_controller.go
@@ -50,7 +50,7 @@ func (r *BoyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	_ = log.FromContext(ctx)
 	_ = log.Log.WithValues("boy", req.NamespacedName)
 	log.Log.Info("app changed", "ns", req.Namespace)
-	log.Log.Info("app changed", "ns", req.String())
+	log.Log.Info("app changed", "ns", req)
 
 	// TODO(user): your logic here
 
