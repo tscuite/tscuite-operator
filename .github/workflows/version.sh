afb68efd6fa7858616195e8d65c91a943d2ba7f8
tscuite <64051240+tscuite@users.noreply.github.com>
2022-05-30 12:41:30 +0800

    Update version.sh

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -1,6 +1,10 @@
-git branch
+NEW_VERSION=$1
+
+echo "curent path: $(pwd), change version to $NEW_VERSION"
+
+          git branch
           git config --global user.name "tscuite"
           git config --global user.email "tscuite@qq.com"
-          git checkout -b pr@20220530@${{github.run_number}}
+          git checkout -b pr@20220530@${NEW_VERSION}
           git remote set-url origin --push --add 'https://tscuite:$cdzGITHUB_TOKEN@github.com/tscuite/tscuite.git'
-          git push --set-upstream origin pr@20220530@${{github.run_number}}
+          git push --set-upstream origin pr@20220530@${NEW_VERSION}
