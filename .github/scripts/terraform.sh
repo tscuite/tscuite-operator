ae3c754a058e9b83a9bf20f60053990513d0878f
tscuite <tscuite@qq.com>
2022-10-10 17:15:49 +0800

    fix: test

---
+++ .github/scripts/terraform.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo git status -s .github  | wc -l
+
+for f in terraform/*; do
+    if [ -d $f ]; then
+        cd $f/prod
+        # This takes into account we always use squash and this runs on push even
+        CHANGES=$(git diff --name-only HEAD..HEAD~1 ../)
+        # if there are any changes run terraform apply
+        if [ "$CHANGES" != "" ]; then
+            echo "detected changes in $f. Running terraform apply..."
+            #terraform init -no-color
+            #terraform apply -no-color -auto-approve
+            echo "$CHANGES"
+        fi
+        cd -
+    fi
+done
